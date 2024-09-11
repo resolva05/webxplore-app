255c2b911ed4f696b15a07f24ff589ed7f9de714
resolva05 <karishma@resolvainsights.com>
2024-09-11 18:43:04 +0530

    footer completed and cta completed

--- navBar/src/App.jsx
+++ navBar/src/App.jsx
@@ -1,53 +1,24 @@
-import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
 // import {
 //   createBrowserRouter,
 //   RouterProvider,
 //   Route,
 //   Link,
 // } from "react-router-dom";
-import logo from "../src/assets/logo.jpg"
+// import logo from "../src/assets/logo.jpg"
 import Footer from './components/Footer';
+import Header from './components/header/Header';
+import Cta from './components/cta';
+// import Footer from './components/Footer';
 
 function App() {
 
   return (
     <>
-      <div>
-
-        <Navbar expand="lg" className="bg-secondary">
-          <Container>
-            <a href="#home">
-              <img src={logo} alt="" style={{ height: "60px", borderRadius: "15px", marginLeft: "10px" }} /></a>
-            <Navbar.Brand href="#home"></Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ms-auto">
-                {/* <Nav.Link href="#home">Home</Nav.Link> */}
-                <Nav.Link href='#about'>About</Nav.Link>
-                <Nav.Link href='#contact'>Contact</Nav.Link>
-                <Nav.Link href='#link'>Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-        <Footer />
-      </div>
+      <Header/>
+      <Cta/>
+      <Footer/>
     </>
 
   )
--- navBar/src/components/header/Header.jsx
+++ navBar/src/components/header/Header.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
-import "./header.css"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from "../../assets/logo.jpg"
+import './Header.css'
 
 const Header = () => {
   return (
-    <div>Header
+    <div>
+
+        <Navbar expand="lg" className="bg-secondary">
+          <Container>
+            <a href="#home">
+              <img src={logo} alt="" style={{ height: "60px", borderRadius: "15px", marginLeft: "10px"}} className='shadow-lg img'/></a>
+              
+            <Navbar.Brand href="#home"></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                {/* <Nav.Link href="#home">Home</Nav.Link> */}
+                <Nav.Link href='#about' className='/' style={{marginRight:"50px", fontSize:"20px", color:""}}>Services</Nav.Link>
+                <Nav.Link href='#contact' className='/' style={{marginRight:"40px", fontSize:"20px"}}>Portfolio</Nav.Link>
+                <Nav.Link href='#link' className='/' style={{marginRight:"30px", fontSize:"20px"}}>Contact Us</Nav.Link>
+                {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown> */}
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <br />
+        Home
+        <br />
         
-    </div>
+      </div>
   )
 }
 
