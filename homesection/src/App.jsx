d1101836a5136d6d8b68dc3310f6892fcabeaa5f
resolva05 <karishma@resolvainsights.com>
2024-09-18 18:58:28 +0530

    18/09/2024 Added blog page layout & sidebar to it

--- homesection/src/App.jsx
+++ homesection/src/App.jsx
@@ -1,7 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header/Header';
 // import Home from './components/home/Home';
-import Contactus from './components/contactus/Contactus';
+// import Contactus from './components/contactus/Contactus';
+import Blogpage from './components/blogpage/Blogpage';
 // import Portfolio from './components/portfolio/Portfolio';
 // import Testimonials from './components/home/Testimonials';
 
@@ -13,7 +14,8 @@ function App() {
   return (
       <div>
         <Header/>
-        <Contactus/>
+        <Blogpage/>
+        {/* <Contactus/> */}
         {/* <Home/> */}
          {/* <Portfolio/> */}
         
