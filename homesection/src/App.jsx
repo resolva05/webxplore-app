3c3e7d2fc81501c18d0b9ff19c6e16618bc04cf0
resolva05 <karishma@resolvainsights.com>
2024-09-23 13:18:14 +0530

    made new branch

---
+++ homesection/src/App.jsx
@@ -0,0 +1,29 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Contactus from './components/contactus/Contactus';
+// import Home from './components/home/Home';
+// import Contactus from './components/contactus/Contactus';
+// import Portfolio from './components/portfolio/Portfolio';
+// import Testimonials from './components/home/Testimonials';
+// import { Outlet } from 'react-router-dom';
+
+function App() {
+
+  
+
+  return (
+      <div>
+        <Contactus/>
+        {/* <Header/>
+        <Blogpage/> */}
+        {/* <Contactus/> */}
+        {/* <Home/> */}
+         {/* <Portfolio/> */}
+        
+         {/* <Testimonials/> */}
+        {/* <Header/> */}
+        {/* <Outlet/> */}
+      </div>
+  )
+}
+
+export default App
