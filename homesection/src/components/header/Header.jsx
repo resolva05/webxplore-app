1320d6e9bd64ab3be95921ad2ecaef5298403524
resolva05 <karishma@resolvainsights.com>
2024-09-30 19:11:47 +0530

    30/09/2024 changing ui

--- homesection/src/components/header/Header.jsx
+++ homesection/src/components/header/Header.jsx
@@ -1,113 +1,62 @@
-// import React from 'react';
+import  { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import logo from "../../assets/logo.jpg"
-import './Header.css'
+import './Header.css';
 import { NavLink } from 'react-router-dom';
-// import searchlogo from '../../assets/search.png'
 import { Form, FormControl } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
-  // const [showSearch, setShowSearch] = useState(false);
+  const [showSearchBox, setShowSearchBox] = useState(false);
 
-  // const toggleSearchBar = () => {
-  //   setShowSearch(!showSearch);
-  // };
-
-  const [showSearch, setShowSearch] = useState(false);
-
-  // Function to toggle search bar visibility
-  const toggleSearchBar = () => {
-    setShowSearch(!showSearch);
+  // Function to toggle search box visibility
+  const toggleSearchBox = () => {
+    setShowSearchBox(!showSearchBox);
   };
+
   return (
-    <div>
-      <Navbar expand="lg" className="" style={{ backgroundColor: "#2b2a29" }}>
+    <div className='head'>
+      <Navbar expand="lg" className="sticky-top" style={{ backgroundColor: "white" }}>
         <Container>
 
-          <img src={logo} alt="" style={{ height: "60px", borderRadius: "15px", marginLeft: "10px" }} className='shadow-lg img' />
-
+          <NavLink to="/home" className="maintext" >Resolva Insights</NavLink>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
           <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
             <Nav className="ms-auto d-flex align-items-center" style={{ flexWrap: "wrap" }}>
-              <NavLink
-                to="/home"
-                className="nav-link"
-                style={{ marginRight: "30px", fontSize: "18px", color: "white", textDecoration: "none" }}
-              >
-                Home
-              </NavLink>
-              <NavLink
-                to="/service"
-                className="nav-link"
-                style={{ marginRight: "30px", fontSize: "18px", color: "white", textDecoration: "none" }}
-              >
-                Services
-              </NavLink>
-              <NavLink
-                to="/portfolio"
-                className="nav-link"
-                style={{ marginRight: "30px", fontSize: "18px", color: "white", textDecoration: "none" }}
-              >
-                Portfolio
-              </NavLink>
-              <NavLink
-                to="/casestudy"
-                className="nav-link"
-                style={{ marginRight: "20px", fontSize: "18px", color: "white", textDecoration: "none" }}
-              >
-                Case Study
-              </NavLink>
-              <NavLink
-                to="/pricing"
-                className="nav-link"
-                style={{ marginRight: "20px", fontSize: "18px", color: "white", textDecoration: "none" }}
-              >
-                Pricing
-              </NavLink>
-              <NavLink
-                to="/contact"
-                className="nav-link"
-                style={{ marginRight: "20px", fontSize: "18px", color: "white", textDecoration: "none" }}
-              >
-                 Contact Us
-              </NavLink>
+              <NavLink to="/home" className="nav-link">Home</NavLink>
+              <NavLink to="/service" className="nav-link">Services</NavLink>
+              <NavLink to="/portfolio" className="nav-link">Portfolio</NavLink>
+              <NavLink to="/casestudy" className="nav-link">Case Study</NavLink>
+              <NavLink to="/pricing" className="nav-link">Pricing</NavLink>
+              <NavLink to="/contact" className="nav-link">Contact Us</NavLink>
 
               {/* Search Icon */}
               <FontAwesomeIcon
                 icon={faSearch}
                 size="lg"
-                onClick={toggleSearchBar}
+                onClick={toggleSearchBox}
                 className="search-icon"
               />
-
-              {/* Search Bar */}
-              <div className={`search-bar-container ${showSearch ? "show" : ""}`}>
-                <Form className="d-flex">
-                  <FormControl
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                  />
-                  <FontAwesomeIcon
-                    icon={faChevronRight}
-                    size="lg"
-                    className="search-submit-icon"
-                  />
-                </Form>
-              </div>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
+
+      {/* Search Box */}
+      <div className={`search-box ${showSearchBox ? 'show' : ''}`}>
+        <Form className="d-flex">
+          <FormControl
+            type="search"
+            placeholder="Search"
+            className="me-2"
+          />
+        </Form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- homesection/src/components/home/Home.jsx
+++ homesection/src/components/home/Home.jsx
@@ -1,13 +1,11 @@
 // import React from 'react'
-import logo from "../../assets/logo.jpg";
 import './Home.css'
 import Services from "./Services";
 import Testimonials from "./Testimonials";
 import { animated, useSpring } from "react-spring";
 import { useState } from "react";
-import bg from '../../assets/heroVideo.mp4'
-import { NavLink } from "react-router-dom";
 import KeyFeatures from "./KeyFeatures";
+import HeroSection from "../heroSection/HeroSection";
 
 const Home = () => {
   const [flip, setFlip] = useState(false);
@@ -20,49 +18,14 @@ const Home = () => {
     onRest: () => setFlip(flip),
   });
   return (
-    <div>
-      <div className="jumbotron">
-        <div className="main">
-          <div className="video-background">
-            <video src={bg} autoPlay loop muted></video>
-          </div>
+    <>
+        
 
           <animated.div style={props}>
-            <div className="d-flex justify-content-between align-items-center">
-              <div>
-                <h1 className="display-4 text-danger">
-                  Welcome to WebXplore Studios!
-                </h1>
-                <p className="lead">
-                  Your Business, Our Expertise, Infinite Possibilities....
-                </p>
-                <img
-                  src={logo}
-                  alt=""
-                  className="img-fluid ml-4 shadow-lg"
-                  style={{
-                    marginRight: "20px",
-                    marginBottom: "50px",
-                    borderRadius: "15px",
-                  }}
-                />
-                <p className="text-slide">
-                  In todays fast-paced world, businesses need to make smarter and
-                  faster decisions. AI-driven decision-making systems leverage advanced
-                  technologies like machine learning and predictive analytics to
-                  transform data into actionable insights. By nt quaerat... Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio eum molestiae alias consequatur veniam necessitatibus excepturi nostrum molestias omnis pariatur assumenda impedit quidem, voluptatum eveniet suscipit, fugit accusamus sapiente voluptatibus.
-                </p>
-              </div>
-            </div>
-            <NavLink to={'./blogpage'}>
-              <button className="btn blog-btn"> Blog Page </button>
-            </NavLink>
+            <HeroSection/>
           </animated.div>
-        </div>
-        <hr className="my-4"></hr>
-        <div className="text-center  fs-1" style={{ fontFamily: "sans-serif" }}>
-          Services Overview
-        </div>
+        
+        
         <Services />
         <hr className="my-4"></hr>
         <br />
@@ -81,9 +44,7 @@ const Home = () => {
           <Testimonials />
         </div>
         <hr className="my-4"></hr>
-      </div>
-    </div>
-
+    </>
   );
 };
 
