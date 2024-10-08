c1d9d6f97a98a25429d82f103b3b9084a7ee58e4
resolva05 <karishma@resolvainsights.com>
2024-10-08 17:17:20 +0530

    08/10/2024- changes in home service section

--- navBar/src/components/home/Testimonials.jsx
+++ navBar/src/components/home/Testimonials.jsx
@@ -27,7 +27,6 @@ const Testimonials = () => {
 
     return () => {
       if (carouselRef.current) {
-        // eslint-disable-next-line react-hooks/exhaustive-deps
         observer.unobserve(carouselRef.current);
       }
     };
@@ -97,12 +96,15 @@ const Testimonials = () => {
                       alt=""
                       style={{ marginTop: "10px", marginLeft: "5px" }}
                     />
-                    <p style={{marginTop:"10px"}}>- CEO, Resolva Insigts Pvt Ltd</p>
                   </p>
+                  <div style={{ marginTop: "10px" }}>
+                    - CEO, Resolva Insights Pvt Ltd
+                  </div>
                 </div>
               </div>
             </div>
           </Carousel.Item>
+
           <Carousel.Item>
             <div className="max-w-full w-[90%] md:w-[300px] rounded-md text-center border-0 mx-auto">
               <div
@@ -121,7 +123,6 @@ const Testimonials = () => {
                 />
               </div>
               <div className="p-4">
-
                 <div
                   className="text-container mx-auto"
                   style={{
@@ -158,15 +159,16 @@ const Testimonials = () => {
                       style={{ marginTop: "10px", marginLeft: "5px" }}
                     />
                   </p>
-                  <p>- Owner of Fashion Boutique Brand</p>
+                  <div>- Owner of Fashion Boutique Brand</div>
                 </div>
               </div>
             </div>
           </Carousel.Item>
+
           <Carousel.Item>
             <div className="max-w-full w-[90%] md:w-[300px] rounded-md text-center border-0 mx-auto">
               <div
-                className="rounded-full overflow-hidden mx-auto mt-2 shadow-lg "
+                className="rounded-full overflow-hidden mx-auto mt-2 shadow-lg"
                 style={{
                   width: "150px", // fixed width
                   height: "150px",
@@ -217,11 +219,12 @@ const Testimonials = () => {
                       style={{ marginTop: "10px", marginLeft: "5px" }}
                     />
                   </p>
-                  <p>- Founder of Food and Beverages</p>
+                  <div>- Founder of Food and Beverages</div>
                 </div>
               </div>
             </div>
           </Carousel.Item>
+
           <Carousel.Item>
             <div className="max-w-full w-[90%] md:w-[300px] rounded-md text-center border-0 mx-auto">
               <div
@@ -269,135 +272,18 @@ const Testimonials = () => {
                     WebXplore Studio delivered. They brought our ideas to life
                     with stunning visuals and an intuitive layout. Our clients
                     appreciate the ease of booking their adventures online, and
-                    we’ve seen a significant uptick in inquiries!
+                    we’ve seen a significant uptick in inquiries.
                     <img
                       src={icon2}
                       alt=""
                       style={{ marginTop: "10px", marginLeft: "5px" }}
                     />
-                    <p style={{marginTop:"10px"}}>- Owner of Travel Firm</p>
                   </p>
+                  <div>- Managing Director of Travel Agency</div>
                 </div>
               </div>
             </div>
           </Carousel.Item>
-          <Carousel.Item>
-            <div className="max-w-full w-[90%] md:w-[300px] rounded-md text-center border-0 mx-auto">
-              <div
-                className="rounded-full overflow-hidden mx-auto mt-2 shadow-lg"
-                style={{
-                  width: "150px", // fixed width
-                  height: "150px",
-                  borderRadius: "50%", // fixed height (same as width for perfect circle)
-                }}
-              >
-                <img
-                  src="https://cdn.pixabay.com/photo/2016/03/31/20/37/client-1295901_640.png"
-                  alt="Laptop Logo"
-                  className="w-full h-full shadow-lg "
-                  style={{ height: "100%" }}
-                />
-              </div>
-              <div className="p-4">
-                <div
-                  className="text-container mx-auto"
-                  style={{
-                    maxWidth: "85%",
-                    padding: "10px",
-                    borderRadius: "10px",
-                    boxSizing: "border-box",
-                  }}
-                >
-                  <p
-                    className="mt-3 text-sm text-gray-600"
-                    style={{
-                      whiteSpace: "normal",
-                      wordWrap: "break-word",
-                      marginRight: "auto",
-                      marginLeft: "auto",
-                      marginBottom: "20px",
-                      width: "85%",
-                    }}
-                  >
-                    <img
-                      src={icon1}
-                      alt=""
-                      style={{ marginBottom: "10px", marginRight: "5px" }}
-                    />
-                    Working with WebXplore Studio on our digital marketing
-                    campaign was a fantastic experience. They provided
-                    innovative solutions and insights that significantly
-                    increased our online engagement. The team is professional,
-                    and their dedication to our success is commendable!
-                    <img
-                      src={icon2}
-                      alt=""
-                      style={{ marginTop: "10px", marginLeft: "5px" }}
-                    />
-                    <p style={{marginTop:"10px"}}>- CMO of Fitness Company</p>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </Carousel.Item>
-          <Carousel.Item>
-            <div className="max-w-full w-[90%] md:w-[300px] rounded-md text-center border-0 mx-auto">
-              <div
-                className="rounded-full overflow-hidden mx-auto mt-2 shadow-lg"
-                style={{
-                  width: "150px", // fixed width
-                  height: "150px",
-                  borderRadius: "50%", // fixed height (same as width for perfect circle)
-                }}
-              >
-                <img
-                  src="https://cdn.pixabay.com/photo/2016/03/31/20/37/client-1295901_640.png"
-                  alt="Laptop Logo"
-                  className="w-full h-full shadow-lg "
-                  style={{ height: "100%" }}
-                />
-              </div>
-              <div className="p-4">
-                <div
-                  className="text-container mx-auto"
-                  style={{
-                    maxWidth: "85%",
-                    padding: "10px",
-                    borderRadius: "10px",
-                    boxSizing: "border-box",
-                  }}
-                >
-                  <p
-                    className="mt-3 text-sm text-gray-600"
-                    style={{
-                      whiteSpace: "normal",
-                      wordWrap: "break-word",
-                      marginRight: "auto",
-                      marginLeft: "auto",
-                      marginBottom: "20px",
-                      width: "85%",
-                    }}
-                  >
-                    <img
-                      src={icon1}
-                      alt=""
-                      style={{ marginBottom: "10px", marginRight: "5px" }}
-                    />
-                    We saw a significant increase in sales after launching our
-                    new e-commerce site. The team at WebXplore Studio exceeded
-                    our expectations!
-                    <img
-                      src={icon2}
-                      alt=""
-                      style={{ marginTop: "10px", marginLeft: "5px" }}
-                    />
-                    <p style={{marginTop:"10px"}}>-	Marketing Director at Software Company</p>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </Carousel.Item>
-          {/* Additional Carousel.Items here */}
         </Carousel>
       </div>
     </>
